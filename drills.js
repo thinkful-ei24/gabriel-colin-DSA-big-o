52d84dd01403b37b6266a505b515bee7e288cba4
Gabriel Morris <gabemorris@outlook.com>
2018-11-06 08:31:17 -0800

    big o drills

---
+++ drills.js
@@ -0,0 +1,35 @@
+// O(1)
+function isEven(value) {
+  if (value % 2 === 0) {
+    return true;
+  } else return false;
+}
+
+// O(n^2), two for loops with one nested
+function areYouHere(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    const el1 = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      const el2 = arr2[j];
+      if (el1 === el2) return true;
+    }
+  }
+  return false;
+}
+
+// O(n), single for loop proportional to length of array
+function doubleArrayValues(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] *= 2;
+  }
+  return array;
+}
+
+// O(n), single for loop loop proportional to the length of the array
+function naiveSearch(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+}
