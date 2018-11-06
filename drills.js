7fb79928258df2520671824e5abec37dd20a15ff
Colin Rupp <colin@thinkful.com>
2018-11-06 08:37:53 -0800

    added more snippets

--- drills.js
+++ drills.js
@@ -33,3 +33,72 @@ function naiveSearch(array, item) {
     }
   }
 }
+
+// O(n^2), two for loops, with one nested in the other
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i+1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+
+
+// O(n), only one for loop
+function generateFib(num) {
+    let result = [];
+  for (let i = 1; i <= num; i++) {
+
+    // we're adding the first item
+    // to the result list, append the
+    // number 0 to results
+    if (i === 1) {
+      result.push(0);
+    }
+    // ...and if it's the second item
+    // append 1
+    else if (i == 2) {
+      result.push(1);
+    }
+
+    // otherwise, sum the two previous result items, and append that value to results.
+    else {
+      result.push(result[i - 2] + result[i - 3]);
+    }
+  }
+  // once the for loop finishes
+  // we return `result`.
+  return result;
+}
+
+
+// O(logn) data is sorted and halved with each iteration
+function efficientSearch(array, item) {
+  let minIndex = 0;
+  let maxIndex = array.length - 1;
+  let currentIndex;
+  let currentElement;
+
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = array[currentIndex];
+    if (currentElement < item) {
+      minIndex = currentIndex + 1;
+    }
+    else if (currentElement > item) {
+      maxIndex = currentIndex - 1;
+    }
+    else {
+      return currentIndex;
+    }
+  }
+  return -1;
+}
+
+
+// O(1) only one operation possible
+function findRandomElement(arr) {
+      return arr[Math.floor(Math.random() * arr.length)];
+}
+
+
